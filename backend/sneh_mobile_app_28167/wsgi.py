8484510ea0f8c90888269626450e6ab96b51ca4d
Crowdbotics <team@crowdbotics.com>
2021-06-22 14:54:07 +0000

    Update models

--- backend/sneh_mobile_app_28167/wsgi.py
+++ backend/sneh_mobile_app_28167/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sneh_mobile_app_28167.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sneh_mobile_app_28167.settings")
 
 application = get_wsgi_application()
